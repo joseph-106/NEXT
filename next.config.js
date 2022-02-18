0db50040dd80f8c9b41e858e4585e866043b60c7
joseph-106 <sjjoseph106@gmail.com>
2022-02-18 16:56:44 +0900

    Redirect & Rewrite

--- next.config.js
+++ next.config.js
@@ -1,3 +1,27 @@
+const API_KEY = process.env.API_KEY;
+
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/contact",
+        destination: "https://github.com/joseph-106",
+        permanent: false,
+      },
+      {
+        source: "/old/:path*",
+        destination: "/new/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
